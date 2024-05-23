fb470e6318abaf3bf2a65db776406804e8194e00
Jason Boutte <boutte.jason@gmail.com>
2024-05-22 22:05:36 -0700

    Removes homeassistant ingress and adds loadbalancerip

--- helmfile/config/homeassistant/postsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "${1}" == "install" ]]; then
-	kubectl -n "${2}" apply -f config/homeassistant/traefik-middleware.yaml
-elif [[ "${1}" == "postuninstall" ]]; then
-	kubectl -n "${2}" delete -f config/homeassistant/traefik-middleware.yaml
-fi
